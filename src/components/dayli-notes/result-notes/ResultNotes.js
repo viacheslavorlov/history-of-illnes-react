c68b3d0f8cf64ede27a83fe36e00dfea2ab33983
viacheslav <viacheslavorlov@gmail.com>
2022-10-14 22:01:49 +0300

    dairy logic basics done 5

--- src/components/dayli-notes/result-notes/ResultNotes.js
+++ src/components/dayli-notes/result-notes/ResultNotes.js
@@ -6,7 +6,7 @@ import './ResultNotes.css';
 import {v4 as uuidv4} from "uuid";
 
 const ResultNotes = () => {
-	const {operationDay, dayIn, dayOut, diagnosis, operation, name} = useSelector(state => state.daylyNotes);
+	const {operationDay, dayIn, dayOut, diagnosis, operation, multipleWounds} = useSelector(state => state.daylyNotes);
 
 	//* dayIn: "2022-10-11"
 	//* dayOut: "2022-10-19"
@@ -18,8 +18,8 @@ const ResultNotes = () => {
 	const formDayList = (end, start) => {
 		const firstDay = new Date(start);
 		const lastDay = new Date(end)
-	    const result = [];
-		const days = (lastDay - firstDay) / 1000 / 60 / 60 /  24;
+		const result = [];
+		const days = (lastDay - firstDay) / 1000 / 60 / 60 / 24;
 		for (let i = 0; i < days; i++) {
 			let date = new Date(firstDay.setDate(firstDay.getDate() + 1))
 			result.push(date);
@@ -33,14 +33,14 @@ const ResultNotes = () => {
 		return result.map((item, i) => {
 			let today = item.toLocaleDateString();
 
-			let min = Math.floor(Math.random() * 10) * 5 ;
+			let min = Math.floor(Math.random() * 10) * 5;
 			if (min < 10) min = '0' + min;
 
 			let doctor = 'лечащего врача';
 			let dayOfOperation = null;
 
-			if (i === result.length - 1 || i === 0 || i === 3) {
-				doctor = 'заведующего отделением';
+			if (i === resultLastIndex || i === 0 || i === 3 || i === 7) {
+				doctor = 'совместно с заведующим отделением';
 			}
 
 			if (today === operDay) {
@@ -50,23 +50,26 @@ const ResultNotes = () => {
 			let complains = () => {
 				switch (i) {
 					case 0:
-						return 'прежние' ;
+						return 'прежние';
 					case 1:
 						return (today > operDay) ?
-							'боли в области послеоперационной(ых) ран(ы)': 'прежние' ;
+							`боли в области послеоперационн${multipleWounds ? 'ых' : 'ой'} ран${multipleWounds ? '' : 'ы'}` : 'прежние';
 					case 2:
 						return (today > operDay) ?
-							'боли в области послеоперационной(ых) ран(ы)': 'прежние' ;
+							`боли в области послеоперационн${multipleWounds ? 'ых' : 'ой'} ран${multipleWounds ? '' : 'ы'}` : 'прежние';
+					case 3:
+						return (today > operDay) ?
+							`боли в области послеоперационн${multipleWounds ? 'ых' : 'ой'} ран${multipleWounds ? '' : 'ы'}` : 'прежние';
 					case resultLastIndex:
 						return 'нет';
 					default:
-						return 'слабые боли в области послеоперационной(ых) ран(ы)';
+						return `слабые боли в области послеоперационн${multipleWounds ? 'ых' : 'ой'} ран${multipleWounds ? '' : 'ы'}`;
 				}
 			}
 
 			const pulse = randomNumber(85, 60);
-			const adH = randomNumberWhithSteps(5, 5, 105);
-			const adL = randomNumberWhithSteps(5, 5, 60);
+			const adH = randomNumberWhithSteps(5, 5, 110);
+			const adL = randomNumberWhithSteps(5, 5, 65);
 			const chDD = randomNumberWhithSteps(5, 1, 13)
 
 			const stul = () => {
@@ -74,36 +77,26 @@ const ResultNotes = () => {
 					case 0:
 						return (today === operDay) ?
 							'Стула не было' : 'Стул';
-					case 1, 2:
+					case 1:
+						return (today > operDay) ?
+							'Стула не было' : 'Стул';
+					case 2:
 						return (today > operDay) ?
 							'Стула не было' : 'Стул';
 					default:
 						return 'Стул оформленный, дефекация умеренно болезенная';
 				}
 			}
-
-
+			console.log('today', today, 'operDay', operDay)
 			const bandage = () => {
-				switch (i) {
-					case 0:
-						return null ;
-					case 1:
-						return (today > operDay) ?
-							'Перевязка с мазью левосин, послеоперационная(ые) рана(ы) без признаков воспаления,' +
-							' \nзаживает(ют) вторичным натяжением': 'прежние' ;
-					case 2:
-						return (today > operDay) ?
-							'Перевязка с мазью левосин, послеоперационная(ые) рана(ы) без признаков воспаления,' +
-							' \nзаживает(ют) вторичным натяжением': 'прежние' ;
-					default:
-						return 'Перевязка с мазью левосин, послеоперационная(ые) рана(ы) без признаков воспаления, ' +
-							'\nзаживает(ют) вторичным натяжением';
-				}
+				return (today > operDay) ?
+					`Перевязка с мазью левосин, послеоперационн${multipleWounds ? 'ые' : 'ая'} ран${multipleWounds ? 'ы' : 'а'} без признаков воспаления, 
+							зажива${multipleWounds ? 'ют' : 'ет'} вторичным натяжением` : '';
 			}
 
 			const discharge = i === resultLastIndex ?
 				`В течение госпитализации выполнена операция ${operation}. 
-				К выписке. Даны рекомендации по режиму, питанию, уходу за  раной(ми).`
+				К выписке. Даны рекомендации по режиму, питанию, уходу за  ран${multipleWounds ? 'ами' : 'ой'}.`
 				: null;
 
 
@@ -115,9 +108,15 @@ const ResultNotes = () => {
 					<div className={"daiary__item__info"}>Жалобы: {complains()}.</div>
 					<div className={"daiary__item__info"}>Состояние удовлетворительное.</div>
 					<div className={"daiary__item__info"}>Кожный покров чистый, обычной окраски.</div>
-					<div className={"daiary__item__info"}>Пульс {pulse} ударов в минуту, ритмичный. АД {adH}/{adL} мм.рт.ст.</div>
-					<div className={"daiary__item__info"}>Дыхание везикулярное, проводится во все отделы. Хрипов нет. ЧДД {chDD} в минуту.</div>
-					<div className={"daiary__item__info"}>Живот не вздут, мягкий, безболезненный, перитонеальных симптомов нет.</div>
+					<div className={"daiary__item__info"}>Пульс {pulse} ударов в минуту, ритмичный.
+						АД {adH}/{adL} мм.рт.ст.
+					</div>
+					<div className={"daiary__item__info"}>Дыхание везикулярное, проводится во все отделы. Хрипов нет.
+						ЧДД {chDD} в минуту.
+					</div>
+					<div className={"daiary__item__info"}>Живот не вздут, мягкий, безболезненный, перитонеальных
+						симптомов нет.
+					</div>
 					<div className={"daiary__item__info"}>{stul()}, диурез в норме.</div>
 					<div className={"daiary__item__diagnosis"}>
 						{doctor === 'заведующего отделением' ? `Диагноз: ${diagnosis}.` : null}
@@ -127,8 +126,8 @@ const ResultNotes = () => {
 					<div className={"daiary__item__info"}>{discharge}</div>
 					<div className="daiary__item__doctor">Врач: Орлов В.И.</div>
 					<div className="daiary__item__doctor">
-						{doctor === 'заведующего отделением' ? 'Заведующий отделением: Богов В.М.' : null}
-						</div>
+						{doctor === 'совместно с заведующим отделением' ? 'Заведующий отделением: Богов В.М.' : null}
+					</div>
 					<div className={"daiary__item__span"}></div>
 				</section>
 			)

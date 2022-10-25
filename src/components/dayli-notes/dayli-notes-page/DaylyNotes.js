a780f86cd3bb12897cf657e5d86bf822b97ff47b
Viacheslav Orlov <viacheslavorlov@gmail.com>
2022-10-25 20:13:51 +0300

    vulnerability fixed

--- src/components/dayli-notes/dayli-notes-page/DaylyNotes.js
+++ src/components/dayli-notes/dayli-notes-page/DaylyNotes.js
@@ -2,8 +2,7 @@ import React from 'react';
 import Header from "../header/Header";
 import FormData from "../form-data/FormData";
 import ResultNotes from "../result-notes/ResultNotes";
-import PrijemnijForm from "../../priemnij/prijemnij-form/PrijemnijForm";
-import PrijemnijStatus from "../../priemnij/prijemnij-status/PrijemnijStatus";
+
 
 import "./DaylyNotes.css";
 

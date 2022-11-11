f784b72d57be3e729d66ec7069c14e81c66ea641
Viacheslav Orlov <viacheslavorlov@gmail.com>
2022-11-11 23:12:35 +0300

    data.json imported works первый тестовый билд создать сохраняемые данные (dexie?)

--- src/components/priemnij/prijemnij-status/PrijemnijStatus.js
+++ src/components/priemnij/prijemnij-status/PrijemnijStatus.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {randomNumber, randomNumberWhithSteps} from "../../../utils/utils";
 import {useSelector} from "react-redux";
 import data from "../../../data/data.json";
+import "./PrijemnijStatus.css";
 
 const PrijemnijStatus = () => {
 	const {
@@ -29,43 +30,48 @@ const PrijemnijStatus = () => {
 					${vk} ${ln} ${new Date(lnDate).toLocaleString().slice(0, 10)}`;
 			case 'не требуется':
 				return 'не требуется';
-			default: return '';
+			default:
+				return '';
 		}
 	}
 
 	return name ? (
-		<div>
-			<h1 className="">Приемный статус</h1>
-			<div>Дата: {date}</div>
-			<div>Жалобы: {complains}.</div>
-			<div>Анамнез заболевания: {anamnesis}.</div>
-			<div>Перенесенные и хронические заболевания: {desieses.join(', ')}</div>
-			<div>Аллергоанамнез: {allergis}.</div>
-			<div>Больничный лист: {lnFinal()}.</div>
-			<div>Употребление алкоголя: отрицает. Курение: отрицает. Заболевания передающиеся половым путём: отрицает. Туберкулёз: отрицает.</div>
-			<div>Состояние удовлетворительное.</div>
-			<div>Кожный покров чистый, обычной окраски.</div>
-			<div>Отёков нет.</div>
-			<div>Пульс {pulse} ударов в минуту, ритмичный. АД {adH}/{adL} мм.рт.ст.</div>
-			<div>Тоны сердца римтичные. Шумов нет.</div>
-			<div>Границы сердца соответствуют возрастной норме.</div>
-			<div>Дыхание везикулярное, проводится во все отделы. Хрипов нет. ЧДД {chDD} в минуту.</div>
-			<div>Живот не вздут, мягкий, безболезненный, перитонеальных симптомов нет.</div>
-			<div>Молочные/грудные железы: симметричны, без дополнительных образований.</div>
-			<div>Местный статус: {statusLocalis}.</div>
-			<div>{stul}, диурез в норме.</div>
-			<div><b>Обоснование диагноза:</b></div>
-			<div>
-				Диагноз установлен на основании жалоб ({complains}), анамнеза ({anamnesis}),
-				данных осмотра ({statusLocalis}).
+		<div className="status">
+			<h1 className="status__header">Приемный статус</h1>
+			<div className="status__date">Дата: {date}</div>
+			<div className="status__data">
+				<div>Жалобы: {complains}.</div>
+				<div>Анамнез заболевания: {anamnesis}.</div>
+				<div>Перенесенные и хронические заболевания: {desieses.join(', ')}</div>
+				<div>Аллергоанамнез: {allergis}.</div>
+				<div>Больничный лист: {lnFinal()}.</div>
+				<div>Употребление алкоголя: отрицает. Курение: отрицает. Заболевания передающиеся половым путём:
+					отрицает. Туберкулёз: отрицает.
+				</div>
+				<div>Состояние удовлетворительное.</div>
+				<div>Кожный покров чистый, обычной окраски.</div>
+				<div>Отёков нет.</div>
+				<div>Пульс {pulse} ударов в минуту, ритмичный. АД {adH}/{adL} мм.рт.ст.</div>
+				<div>Тоны сердца римтичные. Шумов нет.</div>
+				<div>Границы сердца соответствуют возрастной норме.</div>
+				<div>Дыхание везикулярное, проводится во все отделы. Хрипов нет. ЧДД {chDD} в минуту.</div>
+				<div>Живот не вздут, мягкий, безболезненный, перитонеальных симптомов нет.</div>
+				<div>Молочные/грудные железы: симметричны, без дополнительных образований.</div>
+				<div>Местный статус: {statusLocalis}.</div>
+				<div>{stul}, диурез в норме.</div>
+				<div><b>Обоснование диагноза:</b></div>
+				<div>
+					Диагноз установлен на основании жалоб ({complains}), анамнеза ({anamnesis}),
+					данных осмотра ({statusLocalis}).
+				</div>
+				<h3>Клинический диагноз: {diagnosis}</h3>
+				<div><b><em>План обследования:</em></b> {data.plans[diagnosis]}</div>
+				<div><b><em>План лечения:</em></b> {data.cure[diagnosis]}</div>
+				<div><b><em>Врач: Орлов В.И.</em></b></div>
+				<div><b><em>Заведующий отделением: {zavOtdeleniem}</em></b></div>
 			</div>
-			<h3>Клинический диагноз: {diagnosis}</h3>
-			<div><b><em>План обследования:</em></b> {data.plans[diagnosis]}</div>
-			<div><b><em>План лечения:</em></b> {data.cure[diagnosis]}</div>
-			<div><b><em>Врач: Орлов В.И.</em></b></div>
-			<div><b><em>Заведующий отделением: {zavOtdeleniem}</em></b></div>
-		</div>
-	) : null;
-};
+			</div>
+			) : null;
+			};
 
-export default PrijemnijStatus;
+			export default PrijemnijStatus;
